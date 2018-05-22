2177bac0ea7546e470946a17701622ac9c634d1d
hanhpham327 <hanhtp2001@gmail.com>
2018-05-22 12:41:47 -0700

    updated the nav bar

---
+++ src/components/chat.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Chat extends Component {
+    render(){
+        return (
+            <div className="div">
+                <h1 className="center">Chat Room</h1>
+            </div>
+        )
+    }
+}
+export default Chat
